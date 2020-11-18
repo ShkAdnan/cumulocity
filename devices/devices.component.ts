9f4505435fd147185dbea5a1cf723a5f5dc01406
ShkAdnan <AdnanAsad@outlook.com>
2020-11-18 16:59:12 +0500

    Add files via upload

---
+++ devices/devices.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IManagedObject , InventoryService } from '@c8y/client';
+
+@Component({
+    selector: 'app-devices',
+    templateUrl: './devices.component.html'
+  })
+
+  export class DevicesComponent {
+    devices: IManagedObject[] = [];
+    private filter: object = {
+    fragmentType: 'c8y_IsDevice',
+    withTotalPages: true,
+    pageSize: 100
+    };
+    constructor(
+    private inventory:
+    InventoryService
+    ) {
+    this.loadDevices();
+    }
+
+    async loadDevices() {
+    const { data } = await this.inventory.list(this.filter);
+    this.devices = data;
+    console.log(data);
+    }
+}
