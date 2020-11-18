ba29082b5b64b53597362f681bec7944c7eb26e5
ShkAdnan <AdnanAsad@outlook.com>
2020-11-18 17:01:36 +0500

    Add files via upload

---
+++ ngx/ngx.component.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from './data';
+
+@Component({
+  selector: 'app-ngx',
+  templateUrl: './ngx.component.html'
+})
+export class ngxComponent {
+  single: any[];
+  view: any[] = [250, 250];
+  legend: boolean = true;
+  legendPosition: string = 'below';
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  constructor() {
+    Object.assign(this, { single });
+    };
+}
